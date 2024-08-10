9af92f93519ac6470305dce0703e72a0f79ce105
Colton Ehrman <coltonje95@gmail.com>
2024-08-10 16:09:06 -0500

    test(express): include tests with NODE_ENV set differently

--- examples/express/cache-control-header/headers.test.ts
+++ examples/express/cache-control-header/headers.test.ts
@@ -1,18 +1,68 @@
-import {test} from 'vitest';
+import process from 'node:process';
+import {
+	afterEach,
+	describe, expect, test, vi,
+} from 'vitest';
 import express from 'express';
 import request from 'supertest';
 
-test('express sets Cache-Control headers in Response', async () => {
-	const app = express();
-
-	app.get('/healthcheck', (_request, response) => {
-		response.set('Cache-Control', 'max-age=300, must-revalidate');
-
-		response.json({
-			health: 'OK',
-		});
+describe('express headers', () => {
+	afterEach(() => {
+		vi.unstubAllEnvs();
 	});
 
-	await request(app).get('/healthcheck')
-		.expect('Cache-Control', 'max-age=300, must-revalidate');
+	test('NODE_ENV=test express sets Cache-Control headers in Response', async () => {
+		const app = express();
+
+		app.get('/healthcheck', (_request, response) => {
+			response.set('Cache-Control', 'max-age=300, must-revalidate');
+
+			response.json({
+				health: 'OK',
+			});
+		});
+
+		expect(process.env.NODE_ENV).toBe('test');
+
+		await request(app).get('/healthcheck')
+			.expect('Cache-Control', 'max-age=300, must-revalidate');
+	});
+
+	test('NODE_ENV=development express sets Cache-Control headers in Response', async () => {
+		vi.stubEnv('NODE_ENV', 'development');
+
+		const app = express();
+
+		app.get('/healthcheck', (_request, response) => {
+			response.set('Cache-Control', 'max-age=300, must-revalidate');
+
+			response.json({
+				health: 'OK',
+			});
+		});
+
+		expect(process.env.NODE_ENV).toBe('development');
+
+		await request(app).get('/healthcheck')
+			.expect('Cache-Control', 'max-age=300, must-revalidate');
+	});
+
+	test('NODE_ENV=production express sets Cache-Control headers in Response', async () => {
+		vi.stubEnv('NODE_ENV', 'production');
+
+		const app = express();
+
+		app.get('/healthcheck', (_request, response) => {
+			response.set('Cache-Control', 'max-age=300, must-revalidate');
+
+			response.json({
+				health: 'OK',
+			});
+		});
+
+		expect(process.env.NODE_ENV).toBe('production');
+
+		await request(app).get('/healthcheck')
+			.expect('Cache-Control', 'max-age=300, must-revalidate');
+	});
 });
