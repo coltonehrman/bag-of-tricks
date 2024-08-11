0a4086354e3a6a7006e54a4bf158a0a16f7b3fa7
Colton Ehrman <coltonje95@gmail.com>
2024-08-11 15:44:09 -0500

    test: fix test to work with different named roots

--- examples/express/error-stack-trace/error.test.ts
+++ examples/express/error-stack-trace/error.test.ts
@@ -17,35 +17,39 @@ describe('express error', () => {
 		const response = await request(app).get('/');
 
 		const expectedError = `Error: foo
-at /Users/coltonehrman/Desktop/Dev Work/bag-of-tools/examples/express/error-stack-trace/error.test.ts:12:10
-at Layer.handle [as handle_request] (/Users/coltonehrman/Desktop/Dev Work/bag-of-tools/node_modules/.pnpm/express@4.19.2/node_modules/express/lib/router/layer.js:95:5)
-at next (/Users/coltonehrman/Desktop/Dev Work/bag-of-tools/node_modules/.pnpm/express@4.19.2/node_modules/express/lib/router/route.js:149:13)
-at Route.dispatch (/Users/coltonehrman/Desktop/Dev Work/bag-of-tools/node_modules/.pnpm/express@4.19.2/node_modules/express/lib/router/route.js:119:3)
-at Layer.handle [as handle_request] (/Users/coltonehrman/Desktop/Dev Work/bag-of-tools/node_modules/.pnpm/express@4.19.2/node_modules/express/lib/router/layer.js:95:5)
-at /Users/coltonehrman/Desktop/Dev Work/bag-of-tools/node_modules/.pnpm/express@4.19.2/node_modules/express/lib/router/index.js:284:15
-at Function.process_params (/Users/coltonehrman/Desktop/Dev Work/bag-of-tools/node_modules/.pnpm/express@4.19.2/node_modules/express/lib/router/index.js:346:12)
-at next (/Users/coltonehrman/Desktop/Dev Work/bag-of-tools/node_modules/.pnpm/express@4.19.2/node_modules/express/lib/router/index.js:280:10)
-at expressInit (/Users/coltonehrman/Desktop/Dev Work/bag-of-tools/node_modules/.pnpm/express@4.19.2/node_modules/express/lib/middleware/init.js:40:5)
-at Layer.handle [as handle_request] (/Users/coltonehrman/Desktop/Dev Work/bag-of-tools/node_modules/.pnpm/express@4.19.2/node_modules/express/lib/router/layer.js:95:5)`;
+  at <root>/examples/express/error-stack-trace/error.test.ts:12:10
+  at Layer.handle [as handle_request] (<root>/node_modules/.pnpm/express@4.19.2/node_modules/express/lib/router/layer.js:95:5)
+  at next (<root>/node_modules/.pnpm/express@4.19.2/node_modules/express/lib/router/route.js:149:13)
+  at Route.dispatch (<root>/node_modules/.pnpm/express@4.19.2/node_modules/express/lib/router/route.js:119:3)
+  at Layer.handle [as handle_request] (<root>/node_modules/.pnpm/express@4.19.2/node_modules/express/lib/router/layer.js:95:5)
+  at <root>/node_modules/.pnpm/express@4.19.2/node_modules/express/lib/router/index.js:284:15
+  at Function.process_params (<root>/node_modules/.pnpm/express@4.19.2/node_modules/express/lib/router/index.js:346:12)
+  at next (<root>/node_modules/.pnpm/express@4.19.2/node_modules/express/lib/router/index.js:280:10)
+  at expressInit (<root>/node_modules/.pnpm/express@4.19.2/node_modules/express/lib/middleware/init.js:40:5)
+  at Layer.handle [as handle_request] (<root>/node_modules/.pnpm/express@4.19.2/node_modules/express/lib/router/layer.js:95:5)`;
 
 		// Regular expression to match the root part of any UNIX-style file path
 		// The pathRegex is meant to find/replace the `/Users/coltonehrman/Desktop/Dev Work/` parts of the expectedError
 		// The pathRegex is also intended to work when ran on other computers and from different root paths.
 		// Warning: this may or may not work for your local environment
-		const pathRegex = /\/Users(?:\/[^/]+){3}/g;
+		const pathRegex = /\/Users(?:\/[^/]+){4}/g;
 
 		// Transform the error text into a formatted error response that comes from express - express add HTML entities and characters to make it HTML readable
-		const transformedExpectedError = expectedError.replaceAll('\n', '<br> &nbsp; &nbsp;').replaceAll(pathRegex, '<root>');
-		const transformedResponseText = response.text.replaceAll(pathRegex, '<root>');
+		const expectedErrorTransformed = expectedError
+			.replaceAll('\n', '<br>');
 
-		expect(transformedResponseText).toMatch(`<!DOCTYPE html>
+		const actualResponseTextTransformed = response.text
+			.replaceAll(pathRegex, '<root>')
+			.replaceAll('&nbsp;', '');
+
+		expect(actualResponseTextTransformed).toMatch(`<!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="utf-8">
 <title>Error</title>
 </head>
 <body>
-<pre>${transformedExpectedError}</pre>
+<pre>${expectedErrorTransformed}</pre>
 </body>
 </html>
 `);
